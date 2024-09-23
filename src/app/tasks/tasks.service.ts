cd3fc6a726925d6b39c8cb0ad83793af35597f31
Filip Mlýnek <mlynekff@gmail.com>
2024-09-23 15:52:15 +0200

    Local storage for tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,8 +7,15 @@ import {Injectable} from "@angular/core";
   providedIn: 'root'
 })
 export class TasksService {
-  private tasks = DUMMY_TASKS;
+  private tasks: Task[] = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem("tasks")
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   public getUserTasks(userId: string): Task[] {
     return this.tasks.filter(task => task.userId === userId);
@@ -22,11 +29,16 @@ export class TasksService {
       summary: taskRequest.summary,
       dueDate: taskRequest.dueDate,
     })
+    this.saveTasks();
   }
 
   public deleteTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id!== taskId);
+    this.saveTasks();
   }
 
+  private saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  }
 
 }
