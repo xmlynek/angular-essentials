cd2ffebc39c831fdd55430056f0c384eaf4fc8a0
Filip Mlýnek <mlynekff@gmail.com>
2024-09-23 21:07:16 +0200

    More complex module based divided app

--- angular-modules/src/app/app.module.ts
+++ angular-modules/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
-import {TasksComponent} from "./tasks/tasks.component";
-import {BrowserModule} from "@angular/platform-browser";
+import {SharedModule} from "./shared/shared.module";
+import {TasksModule} from "./tasks/tasks.module";
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent
+  ],
   bootstrap: [AppComponent],
-  imports: [HeaderComponent, UserComponent, TasksComponent, BrowserModule]
+  imports: [BrowserModule, SharedModule, TasksModule]
 })
-export class AppModule {}
+export class AppModule {
+}
