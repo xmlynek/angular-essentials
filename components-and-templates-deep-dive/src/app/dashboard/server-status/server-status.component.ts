91b2e8b652e5ac847592969ed264e28f7a2a0e8a
Filip Mlýnek <mlynekff@gmail.com>
2024-10-01 10:01:34 +0200

    ngOnInit

--- components-and-templates-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ components-and-templates-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component";
 
 @Component({
@@ -10,8 +10,36 @@ import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
 
-  protected currentStatus = 'online';
+  protected currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+
+  /**
+   * Initializes the component and sets up a timer to periodically update the server status.
+   *
+   * The server status is randomly determined every 5 seconds and can be one of the following:
+   * - 'online': Represents an active server.
+   * - 'offline': Represents an inactive server.
+   * - 'unknown': Represents an unknown server status.
+   *
+   * @returns {void}
+   */
+  ngOnInit(): void {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+
+
 
 }
